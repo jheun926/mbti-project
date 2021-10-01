5f945aa8d4400de54b16b5a00fa3351edfb0eff6
unknown <jheun926@gmail.com>
2021-10-01 16:58:28 +0900

    first

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,49 @@
 <template>
-  <Tutorial/>
+    <main v-if="page === 0">
+        <img sre="" alt="원피스 캐릭터 이미지" />
+        <h1>나와 닮은 원피스 캐릭터!</h1>
+        <h2>나와 닮은 원피스 캐릭터는 누구일까요?</h2>
+        <Button text="테스트 시작!" :clickEvent="startTest" />
+    </main>
+    <main v-else-if="page < 5">
+        <Question />
+    </main>
 </template>
 
+/* * 메인 페이지 안에 들어갈 것 * 1. 인트로 페이지 * 2. 조건부 렌더링을 통해
+퀴즈 컴포넌트 * 3. 페이지 이동 /result/_mbti */
+
 <script>
-export default {}
+export default {
+    computed: {
+        page() {
+            return this.$store.state.page;
+        },
+    },
+    methods: {
+        startTest() {
+            this.$store.commit("SET_PAGE", 1);
+        },
+    },
+};
 </script>
+
+<style>
+main {
+    padding: 30px 0;
+    width: 100%;
+    max-width: 360px;
+    margin: auto;
+    text-align: center;
+}
+
+h1 {
+    font-size: 1.8em;
+    margin-bottom: 25px;
+}
+
+h2 {
+    font-size: 1.2em;
+    color: #b9b8b7;
+}
+</style>
